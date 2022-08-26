a724a9e385e860604fbd434c064e9b2f84a976b1
Nikodim-monk <lr1975@yandex.ru>
2022-08-26 16:59:14 +0400

    1

--- src/main/java/ru/kata/spring_boot_security/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring_boot_security/configs/WebSecurityConfig.java
@@ -23,7 +23,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(successUserHandler)
@@ -38,10 +40,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public UserDetailsService userDetailsService() {
         UserDetails user =
-                User.withDefaultPasswordEncoder()
+//                User.withDefaultPasswordEncoder()
+                User.builder()
                         .username("user")
-                        .password("user")
+                        .password("{bcrypt}$2y$12$VzkkODgR4Yjqo481YknMde6FoylSHZYzlrOYe4dvjQBfoAHBct2jq")
+//                        .password("user")
                         .roles("USER")
+                        .build();
+        UserDetails admin =
+//                User.withDefaultPasswordEncoder()
+                User.builder()
+                        .username("admin")
+                        .password("{bcrypt}$2y$12$9EDVATpadrV8/1pJjypHO.L70Iyq2Tq7IjD1s/gCkNDocXHYMoEh.")
+//                        .password("admin")
+                        .roles("ADMIN")
                         .build();
 
         return new InMemoryUserDetailsManager(user);
